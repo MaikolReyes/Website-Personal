5b0c4ed855f0277a09797c1c4e07b49a515b0cda
Maikol Reyes <Maikol.Reyes@somosrex.com>
2025-01-17 17:58:28 -0300

    updates in mobile querys

--- src/layout/Hero.jsx
+++ src/layout/Hero.jsx
@@ -5,14 +5,14 @@ export const Hero = () => {
     return (
         <div className="heroImage" id='home' key="name">
 
-            <div className="containerHeroText large-desktop:items-start large-desktop:text-start  mobile:text-center mobile:items-center flex flex-col font-text ml-52 animate__animated animate__backInLeft">
+            <div className="containerHeroText desktop:items-start desktop:text-start  items-center flex flex-col font-text ml-52 animate__animated animate__backInLeft">
 
                 <h2 className="text-saludo text-sm text-white font-title large-desktop:text-base">Hola, Mi nombre es</h2>
 
-                <h2 className='text-name text-3xl text-white large-desktop:text-7xl font-title font-bold'>Maikol Reyes</h2>
+                <h2 className='text-name text-3xl text-white large-desktop:text-7xl font-title font-bold mb-2 mt-2 large-desktop:mt-0 large-desktop:mb-0'>Maikol Reyes</h2>
 
 
-                <p className="text-presentation text-sm large-desktop:w-1/3 mobile:w-3/4 text-white font-title large-desktop:text-base">Soy un progamador especializado en el <span className='textResalt font-title text-customPrimary font-bold'>Desarrollo Frontend</span>, Poseo una sólida formación y un conocimiento integral en el desarrollo de software, lo que me permite abordar desafíos con un enfoque detallado y preciso.</p>
+                <p className="text-presentation text-sm text-start w-3/4 text-white font-title large-desktop:text-base large-desktop:w-1/3">Soy un progamador especializado en el <span className='textResalt font-title text-customPrimary font-bold'>Desarrollo Frontend</span>, Poseo una sólida formación y un conocimiento integral en el desarrollo de software, lo que me permite abordar desafíos con un enfoque detallado y preciso.</p>
                 <div className='containerButtonsPrincipal flex gap-3 mt-3'>
 
                     <a className='buttonsPrincipal btn text-base bg-customPrimary font-title font-bold p-2 w-44 hover:text-black hover:bg-customHover' href="https://wa.link/l5si8o" target='_blank'><i className="fa-brands fa-whatsapp m-1"></i>Whatsapp</a>
