470a9af73ac8866f8ce9737daf66cb02bc9532f3
Maikol Reyes <maikolreyes209@gmail.com>
2025-07-05 20:45:29 -0300

    new projects BI

--- src/lib/projectsAnalyst.js
+++ src/lib/projectsAnalyst.js
@@ -1,6 +1,24 @@
 export const projectsAnalyst = [
     {
     id: 1,
+    name: "Análisis Financiero Empresarial",
+    description:
+      "Este proyecto de Business Intelligence presenta un análisis detallado del comportamiento presupuestario y de gastos dentro de una empresa, utilizando herramientas de inteligencia de negocios y consultas SQL para la creación, exploración, limpieza y visualización de datos.",
+    img: "img/projects/financiero.png",
+    github: "https://github.com/MaikolReyes/Analisis_presupuestario",
+    deploy: '/public/projectsBI/Presupuesto-Empresarial.pbix'
+  },
+  {
+    id: 2,
+    name: "Análisis Crediticio",
+    description:
+      "Este proyecto de Business Intelligence presenta un análisis integral del comportamiento crediticio de clientes, utilizando herramientas de inteligencia de negocios y consultas SQL para la exploración, limpieza y visualización de datos.",
+    img: "img/projects/crediticio.png",
+    github: "https://github.com/MaikolReyes/Analisis_crediticio",
+    deploy: '/public/projectsBI/Analisis-Crediticio.pbix'
+  },
+  {
+    id: 3,
     name: "Análisis de Acciones en Bolsa",
     description:
       "Este proyecto de Business Intelligence tiene como objetivo analizar y comparar el rendimiento de distintas acciones del mercado bursátil utilizando SQL Server y Power BI.",
@@ -9,7 +27,7 @@ export const projectsAnalyst = [
     deploy: '/public/projectsBI/analisis_bolsa.pbix'
   },
   {
-    id: 2,
+    id: 4,
     name: "Análisis de Ventas ElectroMas",
     description:
       "Este proyecto de Business Intelligence fue desarrollado para ElectroMas, una empresa ficticia dedicada a la comercialización minorista de artículos electrónicos y electrodomésticos.",
@@ -18,16 +36,16 @@ export const projectsAnalyst = [
     deploy: '/public/projectsBI/electromas.pbix'
   },
   {
-    id: 3,
+    id: 5,
     name: "Análisis de Sucursales",
     description:
       "Este proyecto consiste en el desarrollo de un tablero interactivo en Power BI que permite analizar y comparar el rendimiento comercial de diferentes sucursales de una empresa a lo largo del tiempo.",
     img: "img/projects/treemap.png",
     github: "https://github.com/MaikolReyes/Analisis-BI-Sucursales",
-      deploy: '/public/projectsBI/analisis-sucursales.pbix'
+    deploy: '/public/projectsBI/analisis-sucursales.pbix'
   },
   {
-    id: 4,
+    id: 6,
     name: "Análisis Inmobiliario",
     description:
       "Este proyecto consiste en la creación de un tablero interactivo desarrollado en Power BI, a partir de un conjunto de datos inmobiliarios.",
@@ -36,7 +54,7 @@ export const projectsAnalyst = [
     deploy: '/public/projectsBI/analisis-inmobiliario.pbix'
   },
   {
-    id: 5,
+    id: 7,
     name: "Análisis de Ventas Retail",
     description:
       "Este proyecto presenta un dashboard interactivo desarrollado en Power BI, orientado al análisis de datos de ventas de una empresa de retail.",
@@ -45,7 +63,7 @@ export const projectsAnalyst = [
     deploy: '/public/projectsBI/analisis-de-ventas-retail.pbix'
   },
   {
-    id: 6,
+    id: 8,
     name: "Análisis Estrategico de Negocio",
     description:
       "Este proyecto tiene como objetivo analizar los datos de ventas de una empresa para obtener conclusiones clave que apoyen la toma de decisiones estratégicas, como la ubicación ideal de un nuevo centro de distribución.",
