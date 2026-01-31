60c1fc04f18fb39b9fe0e1d57fbb8e544d0c766c
Maikol Reyes <Maikol.Reyes@somosrex.com>
2026-01-31 18:04:10 -0300

    change in portfolio

--- src/lib/projectsDev.js
+++ src/lib/projectsDev.js
@@ -1,12 +1,12 @@
 export const projectsDev = [
   {
     id: 1,
-    name: "FinanceSignal",
+    name: "Oliva's Deli",
     description:
-      "Es un sitio web de noticias de finanzas y economía que esta realizado con React, TypeScript, Tailwind y Boostraps. Para el backend utilice strapi para crear, gestionar y distribuir los contenidos del sitio.",
-    img: "./img/projects/finance.png",
-    github: "https://github.com/MaikolReyes/FinanceSignal",
-    deploy: "https://www.financessignal.com/",
+      "Sitio web para emprendimiento de comidas artesanales, diseñado para mostrar productos, servicios de catering y postres. las consultas y pedidos se realizan de forma directa a través de WhatsApp, facilitando una comunicación rápida y personalizada con los clientes.",
+    img: "./img/projects/olivasdelii.png",
+    github: "https://github.com/MaikolReyes/Olivas-Deli",
+    deploy: "https://olivasdeli.com.ar/",
   },
   {
     id: 2,
@@ -19,29 +19,11 @@ export const projectsDev = [
   },
   {
     id: 3,
-    name: "Lexuswatch",
-    description:
-      "Es un E-commerce de venta de relojes smartwatch que esta creado con React, JavaScript, CSS, Boostraps y en el que utilice como DB Firebase para los artículos del sitio web.",
-    img: "./img/projects/lexus.png",
-    github: "https://github.com/MaikolReyes/lexuswatch",
-    deploy: "https://lexuswatch.netlify.app/",
-  },
-  {
-    id: 4,
     name: "Franquicias360",
     description:
       "Es un sitio web para una agencia de marketing de franquicias en el que utilice como lenguaje de programacion React, Javascript y para los estilos de la página utilice el preprocesador SASS.",
     img: "./img/projects/franquicias.png",
     github: "https://github.com/MaikolReyes/Franquicias360",
     deploy: "https://franquicias360.netlify.app/",
-  },
-  {
-    id: 5,
-    name: "Minzke",
-    description:
-      "Es un sitio web de una carrito de compras que esta hecho en HTML, CSS, JavaScript y con el que obtuve mi primer certificación como desarrollador web.",
-    img: "./img/projects/minzke.png",
-    github: "https://github.com/MaikolReyes/Minzke-Ecommerce",
-    deploy: "https://maikolreyes.github.io/Minzke-Ecommerce/",
   },
 ];
